b37ac5893912ce61aca0c587fb0766d9fb94de6c
Robert Zurer <robert@zurer.com>
2020-12-20 06:02:33 -0500

    fixed bad link in iframe

--- app.js
+++ app.js
@@ -9,11 +9,6 @@
   app.set('view engine', 'pug');
   app.use(express.static(path.join(__dirname, 'public')));
   app.use('/', homeController);
-  app.use('*', function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
   app.set('port', process.env.PORT || 6500);
   server = app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + server.address().port);
